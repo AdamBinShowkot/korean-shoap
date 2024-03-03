46638db7c14e5d980492a4c86f5ae68fbab36e63
AdamBinShowkot <adambinshowkot@gmail.com>
2024-03-03 16:51:16 +0600

    Google Adds Script added.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import './globals.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import { 
+  GoogleAnalytics 
+} from '@next/third-parties/google'
 import {
   Row,
   Col
@@ -27,13 +30,37 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/logo.png" sizes="any" />
         <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js" async ></script>
+        {/* <script>
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TZ2KQCGH');
+        </script> */}
+         <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TZ2KQCGH');`
+        }}>
+
+        </script>
+
       </head>
       <body >
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZ2KQCGH"
+          height="0" width="0" style="display:none;visibility:hidden">
+          </iframe>
+        </noscript>
+
         {/* <FlatButton/> */}
           <BodyContainer>
             {children}
           </BodyContainer>
       </body>
+      <GoogleAnalytics gaId="G-FH78GGXT8Z" />
     </html>
   )
 }
