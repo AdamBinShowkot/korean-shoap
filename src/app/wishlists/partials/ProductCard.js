cfcb0cf5ed12e3fbdc69ffd562b2b7d05713e9a9
AdamBinShowkot <adambinshowkot@gmail.com>
2024-03-28 03:58:59 +0600

    Hanlde add to cart on wishlists.

--- src/app/wishlists/partials/ProductCard.js
+++ src/app/wishlists/partials/ProductCard.js
@@ -16,17 +16,22 @@ import {
 import { 
     WishListsContextApi 
 } from '@/contextApi/widhListsContext';
+import { 
+    AddToCartContext 
+} from '@/contextApi/addToCartApi';
 import SuccessToaster from '@/app/ui/SuccessToaster';
 import ErrorToaster from '@/app/ui/ErrorToaster';
 import Image from 'next/image';
-import ConfigureAxios from '@/utils/axiosConfig';
 import WarningModal from '@/app/ui/WarningModal';
+import CartModal from '@/app/shared/CartModal';
+import ConfigureAxios from '@/utils/axiosConfig';
 import axios from 'axios';
 import './index.scss';
 
 
 const ProductCard=({data,IsOdd})=>{
     const {wishLists,setWishLists}=useContext(WishListsContextApi);
+    const {cartLists,setCartLists}=useContext(AddToCartContext);
     const [isSuccess,setIsSuccess]=useState(false);
     const [successMsg,setSuccessMsg]=useState("");
     const [isError,setIsError]=useState(false);
@@ -34,6 +39,107 @@ const ProductCard=({data,IsOdd})=>{
     const [IsShow,setIsShow]=useState(false);
     const [removeData,setRemoveData]=useState({});
     const [errorMsg,setErrorMsg]=useState("");
+    const [show, setShow] = useState(false);
+
+
+    const handleClose = () => setShow(false);
+
+    const handleAddToCart=(infos)=>{
+        //console.log("Data: ",infos);
+
+        const token=localStorage.getItem("token");
+        if(token && infos?.product_id){
+            ConfigureAxios(token);
+            if(infos?.id){
+                let lists =[...cartLists];
+                const currentId=infos.product_id;
+                if(lists?.length){
+                    const index = lists.map(e => parseInt(e.product_id)).indexOf(currentId);
+                    if(index>=0){
+                        const currentProducts=lists[index];
+                        currentProducts.quantity=parseInt(currentProducts.quantity)+1;
+                        const product_id=currentProducts.id;
+                        const obj={
+                            quantity:currentProducts.quantity,
+                            _method:'PUT'
+                        }
+                        axios.post(`/cart/${product_id}`,JSON.stringify(obj))
+                        .then((response)=>{
+                            if(response.status==201){
+                                setShow(true);
+                                setIsSuccess(true);
+                                setSuccessMsg("Add To Cart Success.");
+                                getCartLists(token);
+                                setTimeout(()=>{
+                                    setIsSuccess(false)
+                                },2000)
+                            }
+                        }).catch((error)=>{
+                            setIsError(true);
+                            setErrorMsg("Add to cart failed.")
+                            setTimeout(()=>{
+                                setIsError(false)
+                            },2000)
+                            console.log("Err",error)
+                        })
+                    }else{
+                        const newObj2={
+                            quantity:1,
+                            product_id:infos?.product_id,
+                            product_variant_id:infos.product_variant_id?infos.product_variant_id:0
+                        }
+                        axios.post(`/cart`,JSON.stringify(newObj2))
+                        .then((response)=>{
+                            setShow(true);
+                            setIsSuccess(true);
+                            setSuccessMsg("Add To Cart Success.");
+                            getCartLists(token);
+                            setTimeout(()=>{
+                                setIsSuccess(false)
+                            },2000)
+                        }).catch((error)=>{
+                            setIsError(true);
+                            setErrorMsg("Add to cart failed.")
+                            setTimeout(()=>{
+                                setIsError(false)
+                            },2000)
+                            console.log("CCART",error)
+                        })
+                    }
+                }else{
+                    const newObj={
+                        id:currentId,
+                        product_id:currentId,
+                        image:infos.image,
+                        name:infos.name,
+                        price:parseFloat(variants.price-variants.discount_price).toFixed(0),
+                        quantity:1
+                    }
+                    const newObj2={
+                        quantity:1,
+                        product_id:infos?.id,
+                        product_variant_id:variants.id?variants.id:0
+                    }
+                    setCartLists([...lists,newObj])
+                    axios.post(`/cart`,JSON.stringify(newObj2))
+                    .then((response)=>{
+                        setShow(true);
+                        setAddToCartSuccess(true);
+                        getCartLists(token);
+                        setTimeout(()=>{
+                            setAddToCartSuccess(false)
+                        },2000)
+                    }).catch((error)=>{
+                        setAddToCartError(true);
+                        setTimeout(()=>{
+                            setAddToCartError(false)
+                        },2000)
+                        console.log("CCART",error)
+                    })
+                }
+            }
+        }
+    }
 
     const handleDeleteWishLists=async(data)=>{
         const token=localStorage.getItem("token");
@@ -84,6 +190,21 @@ const ProductCard=({data,IsOdd})=>{
           })
         }
     }
+
+    const getCartLists=async(token="")=>{
+        if(token){
+            ConfigureAxios(token);
+            axios.get(`/cart`)
+            .then((response)=>{
+                if(response.status===200){
+                    setCartLists(response.data)
+                }
+            }).catch((error)=>{
+
+            })
+        }
+
+    }
     return(
         <>
             <Card
@@ -124,6 +245,9 @@ const ProductCard=({data,IsOdd})=>{
                         >
                             <Button
                             className='product-action-button'
+                            onClick={()=>{
+                                handleAddToCart(data);
+                            }}
                             >
                                 Add To Cart
                             </Button> 
@@ -164,6 +288,7 @@ const ProductCard=({data,IsOdd})=>{
             ToastMsg={`${errorMsg?errorMsg:'Product removed from wishlist failed.'}`}
             Width={'20vw'}
             Postion={"bottom-end"}/>
+            <CartModal IsModalShow={show} setIsModalShow={handleClose}/>
         </>
     )
 }
